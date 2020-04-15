feb66478e3ad00c7e006256030e01baa641e4a52
王鹏 <njucmwp@126.com>
2020-04-15 23:28:41 +0800

    [新增]新增用户服务对外api

--- bs-user/bs-user-interface/src/main/java/cn/edu/njucm/wp/bs/user/api/UserApi.java
+++ bs-user/bs-user-interface/src/main/java/cn/edu/njucm/wp/bs/user/api/UserApi.java
@@ -1,4 +1,10 @@
 package cn.edu.njucm.wp.bs.user.api;
 
+import cn.edu.njucm.wp.bs.user.pojo.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public interface UserApi {
+    @GetMapping("query")
+    public User queryUser(@RequestParam("username") String username, @RequestParam("password") String password);
 }
