85858672801452b127e3a3f75f623e9d92c27cdb
王鹏 <wangpeng@taptap.com>
2020-04-15 14:12:02 +0800

    [修改]用户服务结构调整

--- bs-user/bs-user-service/src/main/java/cn/edu/njucm/wp/bs/user/service/UserService.java
+++ bs-user/bs-user-service/src/main/java/cn/edu/njucm/wp/bs/user/service/UserService.java
@@ -1,7 +1,4 @@
 package cn.edu.njucm.wp.bs.user.service;
 
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService {
+public interface UserService {
 }
---
+++ bs-user/bs-user-service/src/main/java/cn/edu/njucm/wp/bs/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package cn.edu.njucm.wp.bs.user.service.impl;
+
+import cn.edu.njucm.wp.bs.user.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
