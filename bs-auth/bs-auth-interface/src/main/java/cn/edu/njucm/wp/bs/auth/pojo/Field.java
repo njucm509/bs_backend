5636b23d73a444845a2ee7a3397ff7697a9aaf17
王鹏 <wangpeng@taptap.com>
2020-04-15 15:58:47 +0800

    [修改]修改role field 数据库字段映射

--- bs-auth/bs-auth-interface/src/main/java/cn/edu/njucm/wp/bs/auth/pojo/Field.java
+++ bs-auth/bs-auth-interface/src/main/java/cn/edu/njucm/wp/bs/auth/pojo/Field.java
@@ -2,6 +2,7 @@ package cn.edu.njucm.wp.bs.auth.pojo;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
 
@@ -9,9 +10,16 @@ import java.time.LocalDateTime;
 @Table(name = "fields")
 public class Field {
     private Long id;
+
     private String sysName;
+
     private String name;
+
     private byte status;
+
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 }
--- bs-auth/bs-auth-interface/src/main/java/cn/edu/njucm/wp/bs/auth/pojo/Role.java
+++ bs-auth/bs-auth-interface/src/main/java/cn/edu/njucm/wp/bs/auth/pojo/Role.java
@@ -2,6 +2,7 @@ package cn.edu.njucm.wp.bs.auth.pojo;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
 
@@ -9,9 +10,16 @@ import java.time.LocalDateTime;
 @Table(name = "roles")
 public class Role {
     private Long id;
+
     private String slug;
+
     private String name;
+
     private String descriptions;
+
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 }
