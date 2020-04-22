d9f550dbcd1af6669fde91a375d2fabf972f142e
王鹏 <njucmwp@126.com>
2020-04-23 01:22:14 +0800

    [修改]init.sql修改

--- db/init.sql
+++ db/init.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.19, for osx10.14 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.18, for osx10.13 (x86_64)
 --
 -- Host: localhost    Database: wp_bs
 -- ------------------------------------------------------
--- Server version	8.0.19
+-- Server version	8.0.18
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -19,7 +19,7 @@
 -- Current Database: `wp_bs`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `wp_bs` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `wp_bs` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 USE `wp_bs`;
 
@@ -31,10 +31,10 @@ DROP TABLE IF EXISTS `fields`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `fields` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `sysName` varchar(50) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `status` tinyint DEFAULT '1',
+  `status` tinyint(4) DEFAULT '1',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -58,7 +58,7 @@ DROP TABLE IF EXISTS `permissions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `permissions` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `slug` varchar(10) NOT NULL,
   `name` varchar(10) NOT NULL,
   `descriptions` varchar(50) DEFAULT NULL,
@@ -85,8 +85,8 @@ DROP TABLE IF EXISTS `role_field`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `role_field` (
-  `role_id` bigint NOT NULL,
-  `field_id` int DEFAULT NULL,
+  `role_id` bigint(20) NOT NULL,
+  `field_id` int(11) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`role_id`)
@@ -110,8 +110,8 @@ DROP TABLE IF EXISTS `role_permission`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `role_permission` (
-  `role_id` bigint NOT NULL,
-  `permission_id` int DEFAULT NULL,
+  `role_id` bigint(20) NOT NULL,
+  `permission_id` int(11) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`role_id`)
@@ -135,15 +135,16 @@ DROP TABLE IF EXISTS `roles`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `roles` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `slug` varchar(10) NOT NULL,
   `name` varchar(50) NOT NULL,
   `descriptions` varchar(50) DEFAULT NULL,
+  `parent_id` int(11) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `roles_slug_uindex` (`slug`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -152,7 +153,7 @@ CREATE TABLE `roles` (
 
 LOCK TABLES `roles` WRITE;
 /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES (2,'admin','管理员',NULL,'2020-04-15 00:36:47','2020-04-15 01:16:46');
+INSERT INTO `roles` VALUES (1,'admin','管理员',NULL,NULL,'2020-04-15 00:36:47','2020-04-15 01:16:46'),(2,'manager','监管者','',NULL,'2020-04-22 15:45:34','2020-04-22 15:45:37'),(3,'provider','数据提供者',NULL,NULL,'2020-04-22 15:46:27','2020-04-22 15:46:31'),(4,'requester','数据请求者',NULL,NULL,'2020-04-22 15:47:18','2020-04-22 15:47:21');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -164,11 +165,10 @@ DROP TABLE IF EXISTS `user_role`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_role` (
-  `user_id` bigint NOT NULL,
-  `role_id` int DEFAULT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `role_id` int(11) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -178,7 +178,7 @@ CREATE TABLE `user_role` (
 
 LOCK TABLES `user_role` WRITE;
 /*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
-INSERT INTO `user_role` VALUES (1,2,NULL,NULL);
+INSERT INTO `user_role` VALUES (1,1,NULL,NULL),(1,2,NULL,NULL),(5,3,'2020-04-22 17:11:49','2020-04-22 17:11:49'),(6,1,'2020-04-22 17:12:52','2020-04-22 17:12:52'),(6,2,'2020-04-22 17:12:52','2020-04-22 17:12:52'),(6,3,'2020-04-22 17:12:52','2020-04-22 17:12:52'),(6,4,'2020-04-22 17:12:52','2020-04-22 17:12:52');
 /*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -190,20 +190,20 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `nickname` varchar(50) DEFAULT NULL,
   `password` varchar(50) NOT NULL,
   `phone` varchar(50) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
-  `ip` int DEFAULT NULL,
-  `flag` tinyint DEFAULT '0',
+  `ip` int(11) DEFAULT NULL,
+  `flag` tinyint(4) DEFAULT '0',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -212,7 +212,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'admin','njucm','',NULL,NULL,NULL,0,NULL,NULL,NULL);
+INSERT INTO `users` VALUES (1,'admin','njucm','',NULL,NULL,NULL,0,NULL,NULL,NULL),(2,'test_wp','njucm','25d55ad283aa400af464c76d713c07ad','18252057910','924613197@qq.com',127001,0,'2020-04-22 09:04:58','2020-04-22 09:04:58',NULL),(3,'test_wp2','njucm','25d55ad283aa400af464c76d713c07ad','18252057910','924613197@qq.com',127001,0,'2020-04-22 09:10:35','2020-04-22 09:10:35',NULL),(4,'test_wp3','njucm','25d55ad283aa400af464c76d713c07ad','18252057910','924613197@qq.com',127001,0,'2020-04-22 09:10:57','2020-04-22 09:10:57',NULL),(5,'test_wp4','njucm','25d55ad283aa400af464c76d713c07ad','18252057910','924613197@qq.com',127001,0,'2020-04-22 09:11:49','2020-04-22 09:11:49',NULL),(6,'test_wp5','njucm','25d55ad283aa400af464c76d713c07ad','18252057910','924613197@qq.com',127001,0,'2020-04-22 09:12:53','2020-04-22 09:12:53',NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -225,4 +225,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-15 18:42:04
+-- Dump completed on 2020-04-23  1:21:41
